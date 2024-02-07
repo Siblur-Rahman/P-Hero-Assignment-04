5309a31ee93515b60eb4a086ea85d85a50df5b3f
MS Rahman <msrahman048gmail.com>
2024-02-07 22:23:14 +0600

    Problem-1 solved

---
+++ a4.js
@@ -0,0 +1,20 @@
+// Problem-1
+function calculateMoney(quantity){
+    const totalExpenditure = 500 + (50*8);
+    if(quantity < 0){
+        return "Input a positive Number!"
+    }else if(quantity >= 0){
+        const totalIncome = quantity * 120;
+        const result = totalIncome - totalExpenditure;
+        return result
+    }
+}
+// Problem-2
+
+// Problem-3
+
+// Problem-4
+
+// Problem-5
+
+
