4e6d2e85de27e95b9cfd22bea0bb8ceb0650c8b6
MS Rahman <msrahman048gmail.com>
2024-02-07 23:38:27 +0600

    Problem-2 solved

--- a4.js
+++ a4.js
@@ -1,16 +1,32 @@
 // Problem-1
-function calculateMoney(quantity){
-    const totalExpenditure = 500 + (50*8);
-    if(quantity < 0){
-        return "Input a positive Number!"
-    }else if(quantity >= 0){
-        const totalIncome = quantity * 120;
-        const result = totalIncome - totalExpenditure;
-        return result
+// function calculateMoney(quantity){
+//     const totalExpenditure = 500 + (50*8);
+//     if(quantity < 0){
+//         return "Input a positive Number!"
+//     }else if(quantity >= 0){
+//         const totalIncome = quantity * 120;
+//         const result = totalIncome - totalExpenditure;
+//         return result
+//     }
+// }
+// Problem-2
+function checkName(name){
+    const lastGoodCharacter = "ayieouw"
+    if(typeof name !== "string" ){
+        return "invalid"
+    }else{
+    const receivedName = name.toLocaleLowerCase()
+    const lastCharacter = receivedName.charAt(receivedName.length-1);
+    if(lastGoodCharacter.includes(lastCharacter) === true){
+        return "Good Name”"
+     }else{
+         return "Bad Name"
+     }
     }
 }
-// Problem-2
-
+// const receivedName = name.toLocaleLowerCase()
+// const lastCharacter = receivedName.charAt(receivedName.length-1);
+console.log(checkName("Salman"))
 // Problem-3
 
 // Problem-4
